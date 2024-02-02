aeb262c8ca6c6ebe4adf2628dad793baa8cff23b
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-02-02 00:47:11 -0500

    added navbar to landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import Image from 'next/image'
+import NavBar from './components/NavBar'
+
 
 export default function Landing() {
   return (
       <>
+        <NavBar />
         <h1>NEXTFLIX</h1>
       </>
   )
