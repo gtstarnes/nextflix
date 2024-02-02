b2912238bb9da26c1779b171a5a4b532318d479d
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-02-02 18:24:37 -0500

    added styles to hero page

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-        <h1>Unlimited movies, TV shows, and more</h1>
-        <p>Watch anywhere. Canel anytime</p>
-        <p>Ready to watch? Enter your email to create or restart your membership</p>
-        <fieldset>
-            <input type="email" id="email" name="email" />
-            <button type="submit">Get Started {'>'}</button>
+    <div className="flex flex-col justify-center items-center h-[50vh]">
+        <div className="flex flex-col justify-center items-center gap-6 text-lg">
+            <h1 className="text-4xl font-bold">Unlimited movies, TV shows, and more</h1>
+            <p>Watch anywhere. Cancel anytime.</p>
+            <p>Ready to watch? Enter your email to create or restart your membership</p>
+        </div>
+        <fieldset className="flex gap-2 mt-5">
+            <input type="email" id="email" name="email" placeholder='Email address'required
+                className="h-16 w-[400px] rounded p-4 border-2 border-gray-400
+                focus:outline-none focus:border-gray-300" 
+                />
+            <button type="submit" 
+                className='bg-red-500 w-[200px] rounded text-2xl font-semibold
+                            hover:bg-red-600' >
+                    Get Started {'>'}
+            </button>
         </fieldset>
     </div>
   )
