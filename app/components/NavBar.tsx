cc6b9eb8f57207f73e4b4be3d877ac654c7e6294
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-02-02 18:03:13 -0500

    played around with minor stuff

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,9 +1,8 @@
 import Link from 'next/link'
-import React from 'react'
 
 
 const NavBar = () => {
-    const isLoggedIn = true;
+    let isLoggedIn = false;
   return (
     <div className="flex justify-between p-8 z-100">
         <h4 className="text-red-500 text-[1.8rem] font-bold">NEXTFLIX</h4>
@@ -14,7 +13,7 @@ const NavBar = () => {
                     <button className="bg-red-500 py-2 px-4 rounded-sm 
                                         hover:rounded-none hover:bg-red-700">
                         <Link href="/login">
-                            Sign in
+                            Sign In
                         </Link>
                     </button>:
                     <button className="bg-red-500 py-2 px-4 rounded-sm 
