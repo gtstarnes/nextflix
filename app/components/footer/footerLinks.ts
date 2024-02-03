64f062184021f40a6e459dcce0b8459bcd049a85
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-02-03 18:48:28 -0500

    created object array for footlink info

---
+++ app/components/footer/footerLinks.ts
@@ -0,0 +1,83 @@
+export const footerLinks = [
+    {
+        text: "FAQ",
+        href: "#",
+    },
+    {
+        text: "Help Center",
+        href: "#",
+    },
+    {
+        text: "Account",
+        href: "#",
+    },
+    {
+        text: "Media Center",
+        href: "#",
+    },
+    {
+        text: "Investor Relations",
+        href: "#",
+    },
+    {
+        text: "Jobs",
+        href: "#",
+    },
+    {
+        text: "Netflix Shop",
+        href: "#",
+    },
+    {
+        text: "Redeem Gift Cards",
+        href: "#",
+    },
+    {
+        text: "Buy Gift Cards",
+        href: "#",
+    },
+    {
+        text: "Ways to Watch",
+        href: "#",
+    },
+    {
+        text: "Terms of Use",
+        href: "#",
+    },
+    {
+        text: "Privacy",
+        href: "#",
+    },
+    {
+        text: "Cookie Preferences",
+        href: "#",
+    },
+    {
+        text: "Corporate Information",
+        href: "#",
+    },
+    {
+        text: "Contact Us",
+        href: "#",
+    },
+    {
+        text: "Speed Test",
+        href: "#",
+    },
+    {
+        text: "Legal Notices",
+        href: "#",
+    },
+    {
+        text: "Only on Netflix",
+        href: "#",
+    },
+    {
+        text: "Do Not Sell or Share My Personal Information",
+        href: "#",
+    },
+    {
+        text: "Ad Choices",
+        href: "#",
+    },
+
+]
