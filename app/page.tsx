2929c6deb939f739138f8973585b7397be2aae6c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-02-06 22:14:21 -0500

    added first card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import EmailSignUpForm from './components/EmailSignUpForm'
 import Hero from './components/Hero'
+import HomeCard from './components/HomeCard'
 import NavBar from './components/NavBar'
 
 
@@ -7,6 +8,14 @@ export default function Home() {
   return (
       <>
         <Hero />
+        <HomeCard>
+          <h1>Enjoy on your TV</h1>
+          <p>Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu ray players, and more.</p>
+        </HomeCard>
+        <HomeCard>
+          <h1></h1>
+          <p></p>
+        </HomeCard>
         <EmailSignUpForm />
       </>
   )
