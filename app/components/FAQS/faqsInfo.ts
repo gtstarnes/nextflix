601fde7653ea9729c281d8a79a2c876c743a605d
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-02-06 22:38:30 -0500

    created FAQS array of objects

---
+++ app/components/FAQS/faqsInfo.ts
@@ -0,0 +1,26 @@
+export const faqs = [
+    {
+        question: 'What is Nextflix?',
+        answer: 'Nextflix is a frontend clone of the popular streaming service Netflix.',
+    },
+    {
+        question: 'How much does Nextflix cost',
+        answer: 'It\'s free because you cannot really watch anything.',
+    },
+    {
+        question: 'Where can I watch?',
+        answer: 'No where',
+    },
+    {
+        question: 'How do I cancel?',
+        answer: 'Don\'t need to',
+    },
+    {
+        question: 'What can I watch on Nextflix?',
+        answer: 'Nothing',
+    },
+    {
+        question: 'Is Nextflix good for kids?',
+        answer: 'Sure, I guess',
+    }
+]
