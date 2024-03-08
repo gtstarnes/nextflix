ffc126194f408252ad3146e81cd3cde4750b6741
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-03-08 00:33:18 -0500

    created basic forgot password page

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const ForgotPassword = () => {
   return (
-    <div>ForgotPassword</div>
+    <div>
+      <h1>Forgot Email/Password</h1>
+      <p>How would you like to reset your password?</p>
+      <form>
+        <input type="checkbox" checked />Email<br/>
+        <input type="checkbox" />Text Message (SMS)<br/>
+
+        <div>
+          <p>We will send you an email with instructions on how to reset your password.</p>
+          <input type="email" placeholder="name@example.com" />
+          <button type="submit">Email Me</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
