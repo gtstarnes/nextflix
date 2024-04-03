db8a8025918ec750853e623999c75dcb6e1fb59b
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-02 23:41:45 -0400

    created types file

---
+++ app/types/types.ts
@@ -0,0 +1,9 @@
+export type Feature = {
+    title?: string,
+    name?: string,
+    release_date: string,
+    overview: string,
+    backdrop_path: string
+    id: number
+    vote_average: number
+}
