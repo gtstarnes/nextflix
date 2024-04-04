e083eb8a8c872d6ea2c8461cd3bc918a982dd0a4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-04 18:20:59 -0400

    added request for row data

--- app/components/Row.tsx
+++ app/components/Row.tsx
@@ -1,10 +1,24 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import type { Feature, Row } from '../types/types'
 
 
 
 const Row = ({title, id, fetchURL}: Row) => {
     const [movies, setMovies] = useState<Feature[]>([])
+
+    useEffect(() => {
+        const requestMovies = async () => {
+            try {
+                const response = await fetch(fetchURL)
+                const data = await response.json();
+                setMovies(data.results)
+            }
+            catch (error) {
+                console.log('Error ', error)
+            }
+        }
+        requestMovies()
+    }, [fetchURL])
   return (
     <div>
         
