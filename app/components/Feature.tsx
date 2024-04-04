e31f7a64e2b9b58a25314550301baeadfb4bf20e
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-04 18:49:46 -0400

    added image for each movie/show

--- app/components/Feature.tsx
+++ app/components/Feature.tsx
@@ -1,9 +1,10 @@
 import type { FeatureType } from "../types/types"
 
-const Feature = (feature: FeatureType) => {
+const Feature = ({show}: {show: FeatureType}) => {
   return (
     <div>
-        
+        <h4>{show.title}</h4>
+        <img src={`https://image.tmdb.org/t/p/original/${show.backdrop_path}`} alt={show.title}/>
     </div>
   )
 }
