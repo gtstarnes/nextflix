33543f9efd5292ad6aa6f20ce803032510fa9723
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-04 19:37:29 -0400

    added more requests for different categories

--- app/requests.ts
+++ app/requests.ts
@@ -2,4 +2,7 @@ const api_key = process.env.NEXT_PUBLIC_MOVIE_API_KEY
 
 export const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`,
+    requestPopularTV: `https://api.themoviedb.org/3/tv/popular?api_key=${api_key}`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}`,
+    requestTopRatedTV: `https://api.themoviedb.org/3/tv/top_rated?api_key=${api_key}`,
 }
