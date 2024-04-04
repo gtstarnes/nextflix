f909baf4e230e3a1deb56af050486e62c90ff23f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-04 19:24:43 -0400

    added flex to each movie; centered title

--- app/components/Feature.tsx
+++ app/components/Feature.tsx
@@ -4,7 +4,7 @@ const Feature = ({show}: {show: FeatureType}) => {
     const title = show.title || show.name
   return (
     <div className="w-[280px] inline-block cursor-pointer relative mx-2">
-        <div className="absolute w-full h-full top-0 left-0 opacity-0 hover:opacity-100 hover:bg-black/60 break-all">
+        <div className="absolute flex items-center justify-center w-full h-full top-0 left-0 opacity-0 hover:opacity-100 hover:bg-black/60 break-all">
             <h4 className="text-xs font-bold">{title}</h4>
         </div>
         <img    
