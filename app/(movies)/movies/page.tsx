80c1afcaec736ad9abae9be81ad7488d0956d046
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-04 18:37:31 -0400

    added row 1 to page

--- app/(movies)/movies/page.tsx
+++ app/(movies)/movies/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 import Main from '@/app/components/Main'
+import Row from '@/app/components/Row'
+import { requests } from '@/app/requests'
 import React from 'react'
 
 const Landing = () => {
   return (
     <div className="">
       <Main  />
+      <Row id='1' title='Popular Movies on NextFlix' fetchURL={requests.requestPopular} />
     </div>
   )
 }
