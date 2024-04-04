e3fb87119b2990e6ad2ffba32f4d9b42442d89d9
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-04 18:41:18 -0400

    imported featuretype, added props

--- app/components/Feature.tsx
+++ app/components/Feature.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import type { FeatureType } from "../types/types"
 
-const Feature = () => {
+const Feature = (feature: FeatureType) => {
   return (
-    <div>Feature</div>
+    <div>
+        
+    </div>
   )
 }
 
