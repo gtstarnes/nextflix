fed17cc4c6d780fccbd928ec28cf2f5d22a3f38a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-05 15:50:19 -0400

    added heart component

--- app/components/Feature.tsx
+++ app/components/Feature.tsx
@@ -1,4 +1,5 @@
 import type { FeatureType } from "../types/types"
+import Heart from "./Heart"
 
 const Feature = ({show}: {show: FeatureType}) => {
     const title = show.title || show.name
@@ -6,6 +7,7 @@ const Feature = ({show}: {show: FeatureType}) => {
     <div className="w-[280px] inline-block cursor-pointer relative mx-2">
         <div className="absolute flex items-center justify-center w-full h-full top-0 left-0 opacity-0 hover:opacity-100 hover:bg-black/60 break-all">
             <h4 className="text-sm font-bold">{title}</h4>
+            <Heart />
         </div>
         <img    
             src={`https://image.tmdb.org/t/p/original/${show.backdrop_path}`} alt={title}
