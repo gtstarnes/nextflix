e7a67fe967f9c1431b6a8a03e60375416b1b9111
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-05 15:55:55 -0400

    set up cosmetic useState for favoriting

--- app/components/Heart.tsx
+++ app/components/Heart.tsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import {FaHeart, FaRegHeart} from 'react-icons/fa'
 
 const Heart = () => {
+    const [favorite, setFavorite] = useState<boolean>(false)
   return (
     <div>
-        <FaRegHeart className="absolute top-2 right-2" size={20} />
+        {favorite ?
+            <FaHeart className="absolute top-2 right-2" size={20} /> :
+            <FaRegHeart className="absolute top-2 right-2" size={20} />
+        }
     </div>
   )
 }
