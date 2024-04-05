11f9a217633e8ea6d5ff01fb04fbdff606a530c6
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-05 17:24:12 -0400

    added request for reality tv

--- app/requests.ts
+++ app/requests.ts
@@ -6,4 +6,6 @@ export const requests = {
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}`,
     requestTopRatedTV: `https://api.themoviedb.org/3/tv/top_rated?api_key=${api_key}`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}`,
+    requestDocumentary: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=99&include_adult=false`,
+    requestReality: `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&with_genres=10763`
 }
