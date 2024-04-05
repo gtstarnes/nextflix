7128e196ef49a7c5dacfad5997437c305ab7d9ed
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-05 17:17:17 -0400

    added documentaries row

--- app/(movies)/movies/page.tsx
+++ app/(movies)/movies/page.tsx
@@ -13,6 +13,7 @@ const Landing = () => {
       <Row id='3' title='Top Rated Movies on NextFlix' fetchURL={requests.requestTopRated} />
       <Row id='4' title='Top Rated Shows on NextFlix' fetchURL={requests.requestTopRatedTV} />
       <Row id='5' title='Coming Soon to a Living Room Near You' fetchURL={requests.requestUpcoming} />
+      <Row id='6' title='Truer Than Fiction' fetchURL={requests.requestDocumentary} />
     </div>
   )
 }
