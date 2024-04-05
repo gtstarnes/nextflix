d498ff19c2d179b22055be55a515ee7454b0cbbf
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-05 15:52:01 -0400

    positioned heart in upper right corner

--- app/components/Heart.tsx
+++ app/components/Heart.tsx
@@ -4,7 +4,7 @@ import {FaHeart, FaRegHeart} from 'react-icons/fa'
 const Heart = () => {
   return (
     <div>
-        <FaRegHeart />
+        <FaRegHeart className="absolute top-2 right-2" size={20} />
     </div>
   )
 }
