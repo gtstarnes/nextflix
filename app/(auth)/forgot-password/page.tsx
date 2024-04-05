11b7d6bf6be9face1ddc1c373b3b62a07dd1534f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-05 17:45:14 -0400

    fixed mistake using checkboxes instead of radial inputs; added conditional rendering based on selection

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,19 +1,42 @@
-import React from 'react'
+"use client"
+
+import {useState} from 'react'
 
 const ForgotPassword = () => {
+  const [option, setOption] = useState<string>('');
+
+  const changeOption = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selection = e.target.value;
+    setOption(selection)
+  }
   return (
-    <div>
+    <div className="mt-20">
       <h1>Forgot Email/Password</h1>
       <p>How would you like to reset your password?</p>
       <form>
-        <input type="checkbox" checked />Email<br/>
-        <input type="checkbox" />Text Message (SMS)<br/>
+        <label htmlFor="email">
+          <input type="radio" id="email" value="email" checked={option === 'email'} onChange={changeOption} />
+          Email
+        </label>
+        <label htmlFor="sms">
+          <input type="radio" id="sms" value="sms" checked={option === 'sms'} onChange={changeOption} />
+          Text Message (SMS)
+        </label>
 
-        <div>
-          <p>We will send you an email with instructions on how to reset your password.</p>
-          <input type="email" placeholder="name@example.com" />
-          <button type="submit">Email Me</button>
-        </div>
+        {option ==='email' ?
+          <div>
+            <p>We will send you an email with instructions on how to reset your password.</p>
+            <input type="email" placeholder="name@example.com" />
+            <button type="submit">Email Me</button>
+          </div> :
+          <div>
+              <p>We will send you a verification code to reset your password. Message and data rates may apply</p>
+              <input type="tel"/>
+              <button type="submit">Text Me</button>
+          </div>
+        }
+
+
       </form>
     </div>
   )
