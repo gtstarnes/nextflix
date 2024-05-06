3e6d88f380cfb8227f1893a8f07223af3768e134
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-05-06 14:46:33 -0400

    added map for reasons

--- app/components/landing/reasons/Reasons.tsx
+++ app/components/landing/reasons/Reasons.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {reasonsInfo} from './reasonsInfo'
+import HomeCard from '../../HomeCard'
 
 const Reasons = () => {
   return (
-    <div>Reasons</div>
+    <HomeCard>
+      <h3>More Reasons to Join</h3>
+      <div>
+        {reasonsInfo.map(reason => {
+          return (
+            <div key={reason.id}>
+              <h2>{reason.reason}</h2>
+              <div>{reason.picture}</div>
+            </div>
+          )
+        })}
+      </div>
+    </HomeCard>
   )
 }
 
