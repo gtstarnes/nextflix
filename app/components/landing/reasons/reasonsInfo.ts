f1eb03bbad0cdb05777a55d9953372d59442ba79
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-05-06 14:41:11 -0400

    created object array for reasonsInfo

---
+++ app/components/landing/reasons/reasonsInfo.ts
@@ -0,0 +1,22 @@
+const reasonsInfo = [
+    {
+        id: 1,
+        reason: "Stories tailored to your taste",
+        picture: '<3',
+    },
+    {
+        id: 2,
+        reason: "Cancel or switch plans anytime",
+        picture: '<3',
+    },
+    {
+        id: 3,
+        reason: "A place just for kids",
+        picture: '<3',
+    },
+    {
+        id: 4,
+        reason: "For your phone, tablet, laptop and TV",
+        picture: '<3',
+    },
+]
