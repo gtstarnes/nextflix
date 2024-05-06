5007856b2abfb1404d23c855463397c1f006658b
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-05-06 15:09:25 -0400

    changed picture to heartsymbol

--- app/components/landing/reasons/reasonsInfo.ts
+++ app/components/landing/reasons/reasonsInfo.ts
@@ -2,21 +2,21 @@ export const reasonsInfo = [
     {
         id: 1,
         reason: "Stories tailored to your taste",
-        picture: '<3',
+        picture: '\u2764',
     },
     {
         id: 2,
         reason: "Cancel or switch plans anytime",
-        picture: '<3',
+        picture: '\u2764',
     },
     {
         id: 3,
         reason: "A place just for kids",
-        picture: '<3',
+        picture: '\u2764',
     },
     {
         id: 4,
         reason: "For your phone, tablet, laptop and TV",
-        picture: '<3',
+        picture: '\u2764',
     },
 ]
