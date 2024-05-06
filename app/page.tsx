002a55be4c82e013a6f9f6131993e63f43169111
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-05-06 15:00:40 -0400

    removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 "use client"
-import EmailSignUpForm from './components/EmailSignUpForm'
 import Faqs from './components/FAQS/Faqs'
 import Hero from './components/Hero'
-import HomeCard from './components/HomeCard'
-import NavBar from './components/NavBar'
 import Reasons from './components/landing/reasons/Reasons'
 
 
