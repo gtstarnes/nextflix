c2b714d45f4556ec5f9a42e8f234fb987b6b0e2a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-05-06 14:11:13 -0400

    added actual answers

--- app/components/FAQS/faqsInfo.ts
+++ app/components/FAQS/faqsInfo.ts
@@ -1,32 +1,32 @@
 export const faqs = [
     {
         question: 'What is Nextflix?',
-        answer: 'Nextflix is a frontend clone of the popular streaming service Netflix.',
+        answer: 'Nextflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you want – all for one low monthly price. There\'s always something new to discover and new TV shows and movies are added every week!',
         id: 1,
     },
     {
         question: 'How much does Nextflix cost',
-        answer: 'It\'s free because you cannot really watch anything.',
+        answer: 'Watch Nextflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from $6.99 to $22.99 a month. No extra costs, no contracts.',
         id: 2,
     },
     {
         question: 'Where can I watch?',
-        answer: 'No where',
+        answer: 'Watch anywhere, anytime. Sign in with your Nextflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Nextflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you\'re on the go and without an internet connection. Take Nextflix with you anywhere.',
         id: 3,
     },
     {
         question: 'How do I cancel?',
-        answer: 'Don\'t need to',
+        answer: 'Nextflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.',
         id: 4,
     },
     {
         question: 'What can I watch on Nextflix?',
-        answer: 'Nothing',
+        answer: 'Nextflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Nextflix originals, and more. Watch as much as you want, anytime you want.',
         id: 5,
     },
     {
         question: 'Is Nextflix good for kids?',
-        answer: 'Sure, I guess',
+        answer: 'The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space. Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.',
         id: 6,
     }
 ]
