64d385b62e4bd4d8952d0f1e6cdbf4d51bac9ce5
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-05-06 14:56:51 -0400

    added padding and alignment styles

--- app/components/landing/reasons/Reasons.tsx
+++ app/components/landing/reasons/Reasons.tsx
@@ -5,13 +5,13 @@ import HomeCard from '../../HomeCard'
 const Reasons = () => {
   return (
     <HomeCard>
-      <h3 className="font-bold">More Reasons to Join</h3>
+      <h3 className="font-bold text-lg mb-2">More Reasons to Join</h3>
       <div>
         {reasonsInfo.map(reason => {
           return (
-            <div key={reason.id} className="flex flex-col">
+            <div key={reason.id} className="flex flex-col justify-between h-[8rem] mb-2 rounded-md bg-slate-700 p-4">
               <h2 className="font-bold text-xl">{reason.reason}</h2>
-              <div className="text-red-500">{reason.picture}</div>
+              <div className="text-red-500 font-extrabold self-end text-lg">{reason.picture}</div>
             </div>
           )
         })}
