028c5d2b204532b291dbd6060206e802401afa89
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-05-06 13:30:58 -0400

    added some basic styles

--- app/components/FAQS/Faqs.tsx
+++ app/components/FAQS/Faqs.tsx
@@ -7,12 +7,12 @@ const Faqs = () => {
     <div>
         {faqs.map(faq => {
           return (
-            <>
-              <button key={faq.question}>{faq.question}</button>
+            <div key={faq.question} className="w-full">
+              <button className="w-full h-10 bg-slate-500 rounded-sm mt-2">{faq.question}</button>
               <div>
-                <p>{faq.answer}</p>
+                <p className="w-full pl-2 mt-1 bg-slate-600">{faq.answer}</p>
               </div>
-            </>
+            </div>
           )
         })}
         <EmailSignUpForm />
