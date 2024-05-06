c119d48d9226e88490d6883118c0756c27b932d4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-05-06 13:54:37 -0400

    removed margin from button

--- app/components/FAQS/Faqs.tsx
+++ app/components/FAQS/Faqs.tsx
@@ -28,7 +28,7 @@ const Faqs = () => {
         {faqs.map(faq => {
           return (
             <div key={faq.id} className="w-full">
-              <button className="w-full h-10 bg-slate-500 rounded-sm mt-1 flex justify-between items-center p-2" onClick={() => changeActivePanel(faq.id)}>{faq.question} <span>{togglePanel(faq.id)}</span></button>
+              <button className="w-full h-10 bg-slate-500 rounded-sm flex justify-between items-center p-2" onClick={() => changeActivePanel(faq.id)}>{faq.question} <span>{togglePanel(faq.id)}</span></button>
               <div>
                 <p className={activePanel === faq.id ? "answer panel-opened" : "answer panel-closed"}>{faq.answer}</p>
               </div>
