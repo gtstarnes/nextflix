d84b5db455fa6748c732c975dd57fefc0b0b2de8
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-05-06 13:32:18 -0400

    set up useState

--- app/components/FAQS/Faqs.tsx
+++ app/components/FAQS/Faqs.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import EmailSignUpForm from '../EmailSignUpForm'
 import { faqs } from './faqsInfo';
 
 const Faqs = () => {
+  const [activePanel, setActivePanel] = useState<PanelType>(null);
   return (
     <div>
         {faqs.map(faq => {
